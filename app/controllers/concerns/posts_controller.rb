fb9023bbcd49cf5bcf7789c686897a1f0c68a509
Jenny Lee & Julianna Rusakiewicz <pair+jl08.julrusak@devbootcamp.com>
2015-09-28 12:01:34 -0400

    initial commit, including routes, controllers, some views

---
+++ app/controllers/concerns/posts_controller.rb
@@ -0,0 +1,29 @@
+class PostsController < ApplicationController
+
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    post = Post.new(posts_params)
+    if post.save
+      redirect_to "/posts/#{post.id}"
+    else
+      redirect_to "/posts/new"
+    end
+  end
+
+  def show
+    @post = Post.find_by(id: params[:id])
+  end
+
+  private
+    def posts_params
+      params.require(:post).permit(:title, :link)
+    end
+
+end
