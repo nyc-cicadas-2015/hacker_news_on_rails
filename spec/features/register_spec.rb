345274923272f87724385c9573ff0d2c02718b21
Jenny Lee & Julianna Rusakiewicz <pair+jl08.julrusak@devbootcamp.com>
2015-09-29 16:51:35 -0400

    Update files to pass regiter tests

--- spec/features/register_spec.rb
+++ spec/features/register_spec.rb
@@ -12,7 +12,6 @@ describe "the sign up process", :type => :feature do
   within("#register") do
     fill_in 'Username', :with => user[:username]
     fill_in 'Password', :with => user[:password]
-    fill_in 'Password Confirmation', :with => user[:password]
   end
     click_button 'Sign up'
     expect(page).to have_content 'Log out'
