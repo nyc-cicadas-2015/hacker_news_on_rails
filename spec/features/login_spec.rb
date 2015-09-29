b5cddb29c222a32591ac85c6d92d84d61f88a90b
Jenny Lee & Julianna Rusakiewicz <pair+jl08.julrusak@devbootcamp.com>
2015-09-29 12:03:59 -0400

    Add logout test using Faker

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -1,13 +1,14 @@
 require_relative '../rails_helper'
 
-describe "the signin process", :type => :feature do
+describe "the login/logout process", :type => :feature do
   before :each do
    create(:user)
+   puts user
   end
 
   let(:user) { attributes_for(:user)}
 
-  it "signs me in" do
+  it "logs me in" do
     visit '/sessions/new'
     within("#session") do
       fill_in 'Username', :with => user[:username]
@@ -16,4 +17,16 @@ describe "the signin process", :type => :feature do
     click_button 'Sign in'
     expect(page).to have_content 'Log out'
   end
+
+  it "logs me out" do
+    visit '/sessions/new'
+    within("#session") do
+      fill_in 'Username', :with => user[:username]
+      fill_in 'Password', :with => user[:password]
+    end
+    click_button 'Sign in'
+    click_link "Log out"
+    expect(page).to have_content "Sign Up"
+  end
+
 end
