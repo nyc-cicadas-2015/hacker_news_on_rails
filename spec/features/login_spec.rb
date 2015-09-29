c5d8b2a215496eeefaf13152df6f7fc637a9602b
Jenny Lee & Julianna Rusakiewicz <pair+jl08.julrusak@devbootcamp.com>
2015-09-29 12:12:16 -0400

    refactor rspec

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -3,28 +3,24 @@ require_relative '../rails_helper'
 describe "the login/logout process", :type => :feature do
   before :each do
    create(:user)
-   puts user
+   log_me_in
   end
 
   let(:user) { attributes_for(:user)}
-
-  it "logs me in" do
+  let(:log_me_in) {
     visit '/sessions/new'
     within("#session") do
       fill_in 'Username', :with => user[:username]
       fill_in 'Password', :with => user[:password]
     end
     click_button 'Sign in'
+  }
+
+  it "logs me in" do
     expect(page).to have_content 'Log out'
   end
 
   it "logs me out" do
-    visit '/sessions/new'
-    within("#session") do
-      fill_in 'Username', :with => user[:username]
-      fill_in 'Password', :with => user[:password]
-    end
-    click_button 'Sign in'
     click_link "Log out"
     expect(page).to have_content "Sign Up"
   end
