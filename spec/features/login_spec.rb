2609e0ffc36909bbc3ae13e2aaa4f46dc2b34b55
Jenny Lee & Julianna Rusakiewicz <pair+jl08.julrusak@devbootcamp.com>
2015-09-30 11:16:47 -0400

    create test for failed login

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -6,7 +6,8 @@ describe "the login/logout process", :type => :feature do
    log_me_in
   end
 
-  let(:user) { attributes_for(:user)}
+  let(:user) { attributes_for(:user) }
+
   let(:log_me_in) {
     visit '/sessions/new'
     within("#session") do
@@ -28,6 +29,17 @@ describe "the login/logout process", :type => :feature do
     click_link "Log out"
     expect(page).to have_content "Sign Up"
   end
-
 end
 
+describe "fail login process", :type => :feature do
+
+  it "throws error when login unsuccessful" do
+    visit'/sessions/new'
+    within("#session") do
+      fill_in 'Username', :with => 'name'
+      fill_in 'Password', :with => 'test'
+    end
+    click_button 'Sign in'
+    expect(page).to have_content "Incorrect username or password"
+  end
+end
